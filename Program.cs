bf07e103aaba4d0fca9b43c094134d21e232e594
Roger Dominguez <rogerd@mail.usf.edu>
2020-01-30 18:05:03 -0500

    Commit Changes

--- Program.cs
+++ Program.cs
@@ -22,13 +22,17 @@ namespace Iterative_Statement
                 Console.WriteLine("Executing a For Loop!");
                 Console.WriteLine("The For Loop will iterate " + value_of_input.ToString() + " times.");
                 // The loop is establisehd here adding to the value of input based off of input + i
-                for (int i = 0; i < value_of_input; i++)
+                for (int i = 1; i <= value_of_input; i++)
                 {
                     // The console reads the final results
                     Console.WriteLine("You have entered: " + value_of_input.ToString() + ". This is the current integer value in the loop: " + i.ToString());
                 }
                 // Ends program and VS studios brings up exit command
                 Console.ReadKey(true);
+            }
+            else
+            {
+                Console.WriteLine(" You have entered an incorrect value");
             }
         }
     }
